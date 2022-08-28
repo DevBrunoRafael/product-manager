9979531b597e2d345c6eeb8e77858f878b2caf52
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-28 12:34:15 -0300

    ...

--- backend_pm/src/main/resources/db/migration/V001__criar-tabela-produtos.sql
+++
@@ -1,7 +0,0 @@
-create table product (
-    id bigint not null auto_increment,
-    name varchar(60) not null,
-    category varchar(20) not null,
-    price decimal(10,2) not null,
-    primary key (id)
-);
